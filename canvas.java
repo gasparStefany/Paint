786a495367c59e0e992f56bc35e53a168806f593
Stefany Gaspar <stefany.franca@sga.pucminas.br>
2020-04-03 23:41:49 -0300

    Adicionar mouse no canvas

--- canvas.java
+++ canvas.java
@@ -1,39 +1,95 @@
-// Java Program to create a to create 
-// a canvas and paint the canvas 
+// Java Program to create a to create a 
+// canvas and mouse listener to the 
+// canvas ( a circle of radius 5 will appear 
+// at the points where mouse are clicked or 
+// dragged on the canvas) 
 import java.awt.*; 
 import javax.swing.*; 
-public class canvas extends JFrame { 
+import java.awt.event.*; 
+
+class canvas extends JFrame implements MouseListener, MouseMotionListener { 
+
+	// create a canvas 
+	Canvas c; 
 
 	// constuctor 
-	canvas() { 
+	canvas() 
+	{ 
 		super("canvas"); 
 
 		// create a empty canvas 
-		Canvas c = new Canvas() { 
-
-			// paint the canvas 
-			public void paint(Graphics g) { 
-				// set color to red 
-				g.setColor(Color.red); 
-
-				// set Font 
-				g.setFont(new Font("Bold", 1, 20)); 
-
-				// draw a string 
-				g.drawString("This is a canvas", 100, 100); 
+		c = new Canvas() { 
+			public void paint(Graphics g) 
+			{ 
 			} 
 		}; 
 
 		// set background 
 		c.setBackground(Color.black); 
 
+		// add mouse listener 
+		c.addMouseListener(this); 
+		c.addMouseMotionListener(this); 
+
 		add(c); 
 		setSize(400, 300); 
 		show(); 
 	} 
 
-	// Main Method 
-	public static void main(String args[]) { 
+	// mouse listener and mouse motion listener mehods 
+	public void mouseClicked(MouseEvent e) 
+	{ 
+		Graphics g = c.getGraphics(); 
+
+		g.setColor(Color.red); 
+
+		// get X and y position 
+		int x, y; 
+		x = e.getX(); 
+		y = e.getY(); 
+
+		// draw a Oval at the point 
+		// where mouse is moved 
+		g.fillOval(x, y, 5, 5); 
+	} 
+
+	public void mouseMoved(MouseEvent e) 
+	{ 
+	} 
+
+	public void mouseDragged(MouseEvent e) 
+	{ 
+		Graphics g = c.getGraphics(); 
+
+		g.setColor(Color.red); 
+
+		// get X and y position 
+		int x, y; 
+		x = e.getX(); 
+		y = e.getY(); 
+
+		// draw a Oval at the point where mouse is moved 
+		g.fillOval(x, y, 5, 5); 
+	} 
+
+	public void mouseExited(MouseEvent e) 
+	{ 
+	} 
+
+	public void mouseEntered(MouseEvent e) 
+	{ 
+	} 
+
+	public void mouseReleased(MouseEvent e) 
+	{ 
+	} 
+
+	public void mousePressed(MouseEvent e) 
+	{ 
+	} 
+	// main class 
+	public static void main(String args[]) 
+	{ 
 		canvas c = new canvas(); 
 	} 
 } 
